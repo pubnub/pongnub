e44e0d4d9a8fd20df5a109aa356d21879e0d8212
Larry Wu <lw7360@gmail.com>
2014-07-08 16:00:05 -0700

    Added leftright.js

---
+++ js/leftright.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    var pubnub = PUBNUB.init({
+        publish_key: 'demo',
+        subscribe_key: 'demo'
+    });
+
+    pubnub.subscribe({
+        channel: 'pongnub_lobby',
+        callback: function(m) {console.log(m)},
+        connect: function() {
+            pubnub.here_now({
+                channel: 'pongnub_lobby',
+                callback: function(m) {
+                    if (m.occupancy >= 3) {
+                        multiPlayer(m);
+                    }
+                    else if (m.occupancy >= 2) {
+                        singlePlayer(m);
+                    }
+                }
+            });
+        }
+    });
+});
+
+var singlePlayer = function(m) {
+    for (var i = 0; i < m.uuids.length; i++) {
+        m.uuids[i]
+    };
+}
+
+var multiPlayer = function(m) {
+
+}
