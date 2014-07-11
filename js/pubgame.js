157068f934e2f174ec2a7c8642a9a1049a34a822
Larry Wu <lw7360@gmail.com>
2014-07-11 09:56:58 -0700

    Added game stuff

--- js/pubgame.js
+++ js/pubgame.js
@@ -1,11 +1,58 @@
 $(document).ready(function () {
-    var pubnub = PUBNUB.init({
+    window.pubnub = PUBNUB.init({
         publish_key: 'demo',
         subscribe_key: 'demo'
     });
 
+    var pongnub = function(m) {
+        if (m.side === "left") {
+            if (m.target === "up") {
+                if (m.type === "touchstart") {
+                    PongGame.leftPaddle.moveUp();
+                }
+                else {
+                    PongGame.leftPaddle.moveDown();
+                }
+            }
+            else if (m.target === "down") {
+                if (m.type === "touchstart") {
+                    PongGame.leftPaddle.moveDown();
+                }
+                else {
+                    PongGame.leftPaddle.stopMovingDown();
+                }
+            }
+        }
+        else if (m.side === "right") {
+            if (m.target === "up") {
+                if (m.type === "touchstart") {
+                    PongGame.rightPaddle.moveUp();
+                }
+                else {
+                    PongGame.rightPaddle.moveDown();
+                }
+            }
+            else if (m.target === "down") {
+                if (m.type === "touchstart") {
+                    PongGame.rightPaddle.moveDown();
+                }
+                else {
+                    PongGame.rightPaddle.stopMovingDown();
+                }
+            }
+        }
+    }
+
+    var presenceHandler = function(m) {
+        if (m.occupancy < 3) { // Someone RQ
+
+        }
+    }
+
     pubnub.subscribe({
         channel: "pongnub_game",
-    })
+        callback: pongnub,
+        presence: presenceHandler
+    });
 
 });
