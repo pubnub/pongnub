858fad499b84c39ad4943124e846bc605dca65c9
Larry Wu <lw7360@gmail.com>
2014-08-15 14:52:55 -0700

    Added utils.js

---
+++ js/utils.js
@@ -0,0 +1,3 @@
+function getURLParameter(name) { // Grabs URL Parameters
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
