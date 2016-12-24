9b31e497871179adcaaa21854b9aad21b0c244d3
Frederick Brock <fred@pubnub.com>
2016-12-23 16:49:35 -0800

    updating QRCode

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,8 @@
 var id = getURLParameter("id");
 $(document).ready(function(){
-    var pubnub = PUBNUB.init({
-        publish_key: 'pub-c-0ecaf3c4-bc3a-4e03-94e7-e85e196fdc4c',
-        subscribe_key: 'sub-c-673a62aa-24c9-11e4-a77a-02ee2ddab7fe'
+    var pubnub = new PubNub(
+        publishKey: 'pub-c-0ecaf3c4-bc3a-4e03-94e7-e85e196fdc4c',
+        subscribeKey: 'sub-c-673a62aa-24c9-11e4-a77a-02ee2ddab7fe'
     });
 
     $("#controls").show();
@@ -24,31 +24,44 @@ $(window).resize(function() {
 
 var initTouchers = function(name, pubnub) {
     var mySide = "left";
-    document.ontouchstart = function(e){ 
-      e.preventDefault(); 
+    document.ontouchstart = function(e){
+      e.preventDefault();
     }
 
     pubnub.subscribe({
-        channel: "pongnub_game",
-        callback: function(m){},
+        channels: ["pongnub_game","pongnub" + id],
         state: {
             side: mySide,
             name: name
         }
     });
-    pubnub.subscribe({
-        channel: "pongnub" + id,
-        callback: function(){},
-        connect: function() {
-          console.log("pongnub" + id);
-        }
-    });
+
+    var listener = {
+      status: function(status){
+        console.log("pongnub" + id);
+      },
+      message: function(m){
+        console.log("message received: " + m);
+      },
+      presence: function(m){
+        console.log("presence received: " + m);
+      }
+
+    };
+
+    var publishCallback = function(status,response){
+      console.log("status: " + status);
+      
+    }
+
+    pubnub.addEventListener(listener);
+
 
     var publishAction = function(action) {
         pubnub.publish({
             channel: "pongnub" + id,
             message: action
-        });
+        }, publishCallback);
     }
 
     var touchHandler = function(eve) {
@@ -71,12 +84,12 @@ var initTouchers = function(name, pubnub) {
     document.addEventListener("touchcancel", function(e) {touchHandler(e);}, false);
     document.addEventListener("mousedown", function(e) {touchHandler(e);}, false);
     document.addEventListener("mouseup", function(e) {touchHandler(e);}, false);
-    document.addEventListener("mouseleave", function(e) {touchHandler(e);}, false);   
+    document.addEventListener("mouseleave", function(e) {touchHandler(e);}, false);
 
     $(document).keydown(function(e) {
       if (e.which == "40") touchHandler({target: {id : "down"}, type: "mousedown" });
       if (e.which == "38") touchHandler({target: {id: "up"}, type: "mousedown" });
-      
+
     });
 
     $(document).keyup(function(e) {
